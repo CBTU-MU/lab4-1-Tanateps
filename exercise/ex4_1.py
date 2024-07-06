518a49f9317cb4f3dfac41b466564276df1aa738
Tanateps <tanateps@hotmail.com>
2024-07-06 04:42:41 +0000

    ex4

--- exercise/ex4_1.py
+++ exercise/ex4_1.py
@@ -6,7 +6,17 @@ def homework():
     import pandas as pd
     data = pd.read_csv("https://raw.githubusercontent.com/toche7/DataSets/main/admit.csv")
     scoreModelNN = None
-    # YOUR CODE HERE
+    
+    y = data.Label
+    X = data[['SubjectA','SubjectB']]
+    from sklearn.neural_network import MLPClassifier
+    nn = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                   hidden_layer_sizes=(10,10),
+                   random_state= 10,
+                   max_iter = 1000,
+                   tol=0.00001)
+    nn.fit(X,y)
+    scoreModelNN = nn.score(X,y)
 
     return scoreModelNN
 
